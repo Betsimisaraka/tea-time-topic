2cf5686b9e1e81379bff855293e228267ac00081
Anita RASOA <anita.ras@onja.org>
2020-10-17 16:37:33 +0300

    Trying to finish the project

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ async function fectchTopics() {
     const response = await fetch(endtoint);
     const data = await response.json();
     topics = data;
-    return topics;
 }
 
 async function displayTopics() {
-    topics = await fectchTopics();
     const sortedDate = topics.sort((a, b) => b.discussedOn - a.discussedOn);
     const html = sortedDate.map(topic => {
         if (topic.discussedOn === "") {
@@ -23,8 +21,13 @@ async function displayTopics() {
              `
                 <li data-id=${topic.id}>
                     <h3>${topic.title}</h3>
-                    <p>${topic.upvotes}</p>
-                    <p>${topic.downvotes}</p>
+                    <button class="archive"></button>
+                    <div class="up_down_votes">
+                        <button data-id="${topic.id}" class="upvotes"></button>
+                        <p>${topic.upvotes}</p>
+                        <button data-id="${topic.id}" class="downvotes"></button>
+                        <p>${topic.downvotes}</p>
+                    </div>
                 </li>
             `
             nextTopics.innerHTML += html;
@@ -35,9 +38,8 @@ async function displayTopics() {
              `
                 <li data-id=${topic.id}>
                     <h3>${topic.title}</h3>
-                    <p>${topic.discussedOn}</p>
-                    <p>${topic.upvotes}</p>
-                    <p>${topic.downvotes}</p>
+                    <p class="discussed">${topic.discussedOn}</p>
+                    <button data-id="${topic.id}" class="delete"></button>
                 </li>
             `
             pastTopics.innerHTML += html;
@@ -70,21 +72,63 @@ function handleSubmit(e) {
 
     const newTopic = {
         discussedOn: "",
-        tille: topicTitle,
+        title: topicTitle,
         id: Date.now(),
         downvotes: 0,
         upvotes: 0,
     }
     topics.push(newTopic);
     console.log(topics);
+    displayTopics(newTopic);
     form.reset();
     container.dispatchEvent(new CustomEvent('storeTopics'));
 }
 
-form.addEventListener('submit', handleSubmit);
+function handleClick(e) {
+    if (e.target.closest('.delete')) {
+        let liElement = e.target.closest('li');
+        const deleteBtn = liElement.querySelector('button.delete') 
+        const id = deleteBtn.dataset.id;
+        deleteTopic(id);
+    }
+    if (e.target.closest('.upvotes')) {
+        const closer = e.target.closest('li');
+        const button = closer.querySelector('button.upvotes');
+        const id = button.dataset.id;
+        upvotesBtn(id);
+    }
+    if (e.target.closest('.downvotes')) {
+        const button = e.target.closest('.downvotes');
+        console.log(button);
+        const id = button.dataset.id;
+        downvoteBtn(id);
+    }
+}
 
+function upvotesBtn(idToUp) {
+    console.log(idToUp, 'upvotes');
+    const topic = topics.find(topic => topic.id == idToUp)
+    topic.upvotes++;
+    container.dispatchEvent(new CustomEvent('storeTopics'));
+}
+
+function downvoteBtn(idToUp) {
+    console.log(idToUp, 'downvotes');
+    const topic = topics.find(topic => topic.id == idToUp)
+    topic.downvotes++;
+    container.dispatchEvent(new CustomEvent('storeTopics'));
+}
+
+function deleteTopic(idTodelete) {
+    console.log(idTodelete, 'deleted');
+    topics = topics.filter(topic => topic.id != idTodelete);
+    container.dispatchEvent(new CustomEvent('storeTopics'));
+}
+
+form.addEventListener('submit', handleSubmit);
 container.addEventListener('storeTopics', displayTopics);
 container.addEventListener('storeTopics', updateLocalStorage);
+container.addEventListener('click', handleClick);
 
-displayTopics();
-initLocalStorage();
+displayTopics()
+// initLocalStorage();
