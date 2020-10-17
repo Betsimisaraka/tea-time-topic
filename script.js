a1d2ee440b32faece34d4e1c95a935c42b49b0dc
Anita RASOA <anita.ras@onja.org>
2020-10-17 11:15:52 +0300

    Storing the topics in the local storage

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const endtoint = "https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c703
 const nextTopics = document.querySelector('.next_topic');
 const pastTopics = document.querySelector('.past_topics');
 const form = document.querySelector('form');
+const container = document.querySelector('.dispalyTopics');
 
+let topics = [];
 
 async function fectchTopics() {
     const response = await fetch(endtoint);
     const data = await response.json();
-    return data;
+    topics = data;
+    return topics;
 }
 
 async function displayTopics() {
-    const topicsToDisplay = await fectchTopics();
-    const sortedDate = topicsToDisplay.sort((a, b) => b.discussedOn - a.discussedOn);
+    topics = await fectchTopics();
+    const sortedDate = topics.sort((a, b) => b.discussedOn - a.discussedOn);
     const html = sortedDate.map(topic => {
         if (topic.discussedOn === "") {
             const html =
@@ -44,22 +47,43 @@ async function displayTopics() {
     return html;
 }
 
+function initLocalStorage() {
+    const topicsFromLs = localStorage.getItem('topics');
+    const items = JSON.parse(topicsFromLs); 
+    if (items) {
+        topics = items;
+        container.dispatchEvent(new CustomEvent('storeTopics'));
+    } else {
+        topics = [];
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('topics', JSON.stringify(topics))
+}
+
 function handleSubmit(e) {
     console.log(e.target);
     e.preventDefault();
     const form = e.target;
     const topicTitle = form.teatopic.value;
 
-    const newTopic = `
-        <li>
-            <p>${topicTitle}</p>
-        </li>
-    
-    `
-    nextTopics.insertAdjacentHTML('beforeend', newTopic);
+    const newTopic = {
+        tille: topicTitle,
+        id: Date.now(),
+        downvotes: 0,
+        upvotes: 0,
+    }
+    topics.push(newTopic);
+    console.log(topics);
+    form.reset();
+    container.dispatchEvent(new CustomEvent('storeTopics'));
 }
 
 form.addEventListener('submit', handleSubmit);
 
+container.addEventListener('storeTopics', displayTopics);
+container.addEventListener('storeTopics', updateLocalStorage);
+
 displayTopics();
-// fectchTopics();
+initLocalStorage();
